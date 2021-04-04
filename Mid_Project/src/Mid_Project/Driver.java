02c365060b7b04e70a1e7053a79e19652f3435ae
Khadija-Asif <81873642+Khadija-Asif@users.noreply.github.com>
2021-04-04 17:39:41 +0500

    created First JFrame

--- Mid_Project/src/Mid_Project/Driver.java
+++ Mid_Project/src/Mid_Project/Driver.java
@@ -9,14 +9,15 @@ package Mid_Project;
  *
  * @author KHADIJA
  */
-public class Test1 {
+public class Driver{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+        JFrame1 f = new JFrame1();
+        f.setVisible(true);
     }
     
 }
